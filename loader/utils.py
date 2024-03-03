16a493a1af9fca34da2a6e025f7011aab997491b
ChaejinE <joung6517@gmail.com>
2024-03-03 18:05:25 +0900

    add loader test

--- loader/utils.py
+++ loader/utils.py
@@ -12,13 +12,12 @@ def download_file_from_url(url: str, save_path: str) -> None:
             with open(save_path, "wb") as f:
                 f.write(response.content)
             if os.path.exists(save_path):
-                logging.info(f"Download Success on {save_path}")
+                logging.info(f"{__file__}\nDownload Success on {save_path}")
             else:
                 raise RuntimeError
         else:
             raise RuntimeError
     except Exception as e:
         logging.warning(
-            f"Download Fail from {url}\nResponse Status : {response.status_code}"
+            f"{__file__}\nDownload Fail from {url}\nResponse Status : {response.status_code}\n{e}"
         )
-        logging.warning(e)
---
+++ services/thesis_summary/test/test_load_pdf.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from loader.thesis_summary import ThesisSummaryLoader
+from loader.utils import download_file_from_url
+from langchain_core.documents.base import Document
+from langchain_logger import logger
+
+
+class LoadPdfTest(TestCase):
+    def setUp(self) -> None:
+        url = "https://arxiv.org/pdf/1706.03762.pdf"
+        self._pdf_path: str = "./transformer.pdf"
+        download_file_from_url(url=url, save_path=self._pdf_path)
+        self._loader = ThesisSummaryLoader(file_path=self._pdf_path)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        import os
+
+        os.remove(self._pdf_path)
+        return super().tearDown()
+
+    def test_load(self):
+        content = self._loader.load()
+        self.assertIsInstance(content, list)
+        if content and len(content) > 0:
+            self.assertIsInstance(content[0], Document)
+        else:
+            logger.info(f"{__file__}\nDocument Length is : {len(content)}")
