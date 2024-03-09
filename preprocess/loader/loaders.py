daaa571a628d47f9243422a567d933ba797e628e
ChaejinE <joung6517@gmail.com>
2024-03-09 14:04:11 +0900

    update pkg path

--- preprocess/loader/loaders.py
+++ preprocess/loader/loaders.py
@@ -1,9 +1,9 @@
 from preprocess.loader.base import BaseLoader
+from preprocess.loader.utils import download_file_from_url
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents.base import Document
 from engine_logger.langchain_logger import logger
-from loader.utils import download_file_from_url
 
 import sys
 
