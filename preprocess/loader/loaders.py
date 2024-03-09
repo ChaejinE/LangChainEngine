e85dd21a23f231a455f5e2092231a216717843e0
ChaejinE <joung6517@gmail.com>
2024-03-09 14:03:37 +0900

    updtae pkg path

--- preprocess/loader/loaders.py
+++ preprocess/loader/loaders.py
@@ -1,4 +1,4 @@
-from loader.base import BaseLoader
+from preprocess.loader.base import BaseLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents.base import Document
