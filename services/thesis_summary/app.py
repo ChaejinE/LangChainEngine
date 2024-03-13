d4786e0c0734e2f70e515fe256609b0a2af1f0cc
ChaejinE <joung6517@gmail.com>
2024-03-13 23:48:21 +0900

    add CORS

--- services/thesis_summary/app.py
+++ services/thesis_summary/app.py
@@ -5,12 +5,27 @@ logging.basicConfig(level=logging.WARNING)
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from langserve import add_routes, CustomUserType
 from chain.chains import ThesisSummaryChain
 from langchain_core.runnables import RunnableLambda
 from langchain.pydantic_v1 import Field
 
 app = FastAPI(title="Thesis Summary Server")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ThesisSummaryReuqest(CustomUserType):
@@ -53,7 +68,11 @@ def main(request: ThesisSummaryReuqest) -> str:
     return result
 
 
-add_routes(app, RunnableLambda(main), path="/summary")
+add_routes(
+    app,
+    RunnableLambda(main).with_types(input_type=ThesisSummaryReuqest, output_type=str),
+    path="/summary",
+)
 
 if __name__ == "__main__":
     import uvicorn
